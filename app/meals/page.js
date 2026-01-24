1589e296b5bc09bb73fdd05d4e1023a0df103e5f
kevin tanes <kevin.tanes@gmail.com>
2026-01-24 16:52:11 +0700

    feat: add meals content, add new component meal-item, and meals-grid, install new package sqlite

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,18 +1,22 @@
 import Link from "next/link";
-import React from "react";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 const MealsPage = () => {
   return (
     <>
-      <h1>This is meals page</h1>
-      <ul>
-        <li>
-          <Link href={`/meals/detail-1`}>Meal Detail 1</Link>
-        </li>
-        <li>
-          <Link href={`/meals/detail-2`}>Meal Detail 2</Link>
-        </li>
-      </ul>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Receipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 };
