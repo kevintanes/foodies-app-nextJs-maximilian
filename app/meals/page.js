ded98afee31d78350d138ba7d6a53afe5011fa94
kevin tanes <kevin.tanes@gmail.com>
2026-01-26 23:36:39 +0700

    feat: init db, add loading when fetching data, using suspense from react for loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,14 @@
-import Link from "next/link";
-import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
+import { Suspense } from "react";
+import classes from "./page.module.css";
+
+const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
 
 const MealsPage = () => {
   return (
@@ -15,7 +23,9 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
