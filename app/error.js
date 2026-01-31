091bad5c343f20aef35462097ef1cc8734f2cd46
kevin tanes <kevin.tanes@gmail.com>
2026-01-31 12:19:43 +0700

    feat: add error and not found page, add meal detail page content, and fetching meal detail

---
+++ app/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+const Error = () => {
+  <main className="error">
+    <h1>An error occured!</h1>
+    <p>Failed to fetch meal. Please try again later.</p>
+  </main>;
+};
+
+export default Error;
--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,42 @@
 import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-const MealDetailPage = async ({ params }) => {
-  const { slug } = await params;
-  return <h1>This is meal detail page with id: {slug}</h1>;
+const MealDetailPage = ({ params }) => {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        />
+      </main>
+    </>
+  );
 };
 
 export default MealDetailPage;
